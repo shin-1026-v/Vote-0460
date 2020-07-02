ffa3c36aa25c7e091083e99e350fd45eaa6eaeb6
上松 真也 <s.u@shinya-MacBook-Pro.local>
2020-07-02 15:44:20 +0900

    basic-login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :users
 
-  get '/signup', to: 'users#new'
+  get    '/signup',   to: 'users#new'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
 
 end
